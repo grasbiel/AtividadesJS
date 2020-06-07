ef2c654311e2e545c134a66377f652cacda6924d
bzsilva1998 <gabrielbs1998@hotmail.com>
2020-06-06 21:46:34 -0300

    Atualização de JS e CSS

--- Calculadora/js/script.js
+++ Calculadora/js/script.js
@@ -1,38 +1,25 @@
-function calculadora() {
-  var num1 = window.document.getElementById('num1')
-  var num2 = window.document.getElementById('num2')
-  var opcoes = window.document.getElementsByName('opcao')
-  var opcao;
+
   
-  for (var i=0; i<opcoes.length; i++) {
-    if (opcoes[i].checked) {
-      opcao = Number(opcoes[i].value);
+
+function calcular (tipo, valor) {
+  if (tipo== 'operacao') {
+    if(valor=='c') {
+      document.getElementById('resultado').value = ''
     }
+
+    else if (valor=='+' || valor =='-' || valor == '/' || valor == '*' || valor == '.') {
+      document.getElementById('resultado').value += valor
+    }
+
+    else if (valor ==='calc') {
+      var calcula = eval(document.getElementById('resultado').value)
+      document.getElementById('resultado').value = calcula
+    }
+
   }
 
-  var res = window.document.getElementById('res')
-  var n1 = Number(num1.value)
-  var n2 = Number(num2.value)
-  
-  switch (opcao) {
-    case 1:
-      result = n1 + n2
-      res.innerText = ` ${result} `
-      break;
-
-    case 2:
-      result = n1 - n2
-      res.innerText = ` ${result} ` 
-      break;
-
-    case 3:
-      result = n1 * n2
-      res.innerHTML = `${result} `
-      break;
-
-    case 4:
-      result = n1 / n2
-      res.innerHTML = ` ${result} `
-      break;
+  else {
+    document.getElementById('resultado').value += valor
+    
   }
 }
