628f8d2d692fe54ac8f523c78ff2e10895ea08c1
bzsilva1998 <gabrielbs1998@hotmail.com>
2021-01-19 22:39:22 -0300

    Jogo Mata Mosquito

--- Mata Mosquito/jogo.js
+++ Mata Mosquito/jogo.js
@@ -1,5 +1,33 @@
+var altura = 0
+var largura = 0
 
+function ajustaTamanhoPalcoJogo () {
+    altura = window.innerHeight
+    largura = window.innerWidth
+}
 
+ajustaTamanhoPalcoJogo()
 
-var altura = window.screen.availHeight
-var largura = window.screen.availWidth    
+function posicaoRandomica () {
+    var posicaoX = Math.floor(Math.random() * largura) - 90
+    var posicaoY = Math.floor(Math.random() * altura) - 90
+
+    poiscaoX = posicaoX < 0 ? 0 : posicaoX
+    posicaoY = posicaoY < 0 ? 0 : posicaoY
+
+    var mosquito = document.createElement('img')
+    mosquito.src = 'imagens/mosca.png'
+    mosquito.className = 'mosquito1'
+    mosquito.style.left = posicaoX + 'px'
+    mosquito.style.top = posicaoY + 'px'
+    mosquito.style.position = 'absolute'
+    
+    document.body.appendChild(mosquito)
+
+    tamanho
+}
+
+function tamanhoAleatorio () {
+    var classe = Math.floor(Math.random()*3)
+    
+}
